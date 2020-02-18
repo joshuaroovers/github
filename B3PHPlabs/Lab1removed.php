949a6427897eb6a841250246a1b216c5b8c2744e
Joshua <Doerak1aug@gmail.com>
2020-02-18 11:11:27 +0100

    .

---
+++ B3PHPlabs/Lab1removed.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="nl">
+    <head>
+
+    </head>
+
+    <body>
+    <?php
+
+    /* $variable = 0;
+    $string = "hehehehehe";
+    $IAmCaseSensitive = "that's fair";
+    $arrrrrray = array("A" => 0, "B" => 0, "C" => 0);
+    $brrrrrby = ["appel", "naan", "lifeDidntGiveYouLemonsHumansDid"];
+
+
+
+    $sentence = $string ." ". $brrrrrby[1] ."<br>";
+    $sameSentence = $string ." ". $brrrrrby[1] ."<br>"; #you can also use .= if you dont use any loose strings like i did #but like why bother
+
+    define("vArIaBlE", 1.5, true); #withe the true at the end the variable name isnt case sensitive. also #coments
+    $variable = 1.5;
+
+    echo($sentence);
+    print($sameSentence . $sameSentence); #echo is beter. I think? sounds cooler at least
+    echo($variable . " " . $IAmCaseSensitive); 
+
+    echo"<h1>DIT IS EEN H1</h1>" #can also do <script></script> for javascripts :3
+    */
+    
+    ?>
+
+    </body>
+
+</html>
