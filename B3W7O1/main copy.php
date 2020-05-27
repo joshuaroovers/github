868c98e34cbee7d414b70890d2c0ef359e7f8114
Joshua <Doerak1aug@gmail.com>
2020-05-27 15:33:35 +0200

    fenageled with the images now they are all squares

---
+++ B3W7O1/main copy.php
@@ -0,0 +1,52 @@
+<?php
+    $sql = new PDO("mysql:host=localhost;dbname=gamesx", 'root', '');
+    $stmt = $sql->prepare("SELECT * FROM games");
+    $stmt->execute(); 
+    $database[] = $stmt->fetchAll();
+
+    $sql = new PDO("mysql:host=localhost;dbname=gamesx", 'root', '');
+    $stmt = $sql->prepare("SELECT COUNT(image) FROM games");
+    $stmt->execute();
+    $imagecount = $stmt->fetchcolumn();
+    
+    
+?>
+
+<!DOCTYPE html>
+<html lang = "nl">
+
+    <head>
+        <script src="https://kit.fontawesome.com/89f724c9b8.js" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <link rel ="stylesheet" href="style.css">
+    </head>
+
+    <body>
+        <div id = "wrapper">
+            <ul class = "nav">
+                <li class = "nav-item"><a class = "nav-link"><i class="fas fa-chevron-left"></i></a></li>
+                <li class = "nav-item"><a class = "nav-link">pagina 1</a></li>
+                <li class = "nav-item"><a class = "nav-link">pagina 2</a></li>
+                <!-- <i class="fas fa-chevron-left"></i> -->
+            </ul>
+            <div>
+                <span><i class="far fa-square"></i></span>
+                <span><i class="far fa-square"></i></span>
+            </div>
+            <div>
+                <?php
+
+                    for($x = 0; $x < $imagecount; $x++)
+                    {
+                        $images[$x] = $database[0][$x]["image"];
+                    }
+
+                    foreach ($images as $b) {
+                        echo("<img src=\"./afbeeldingen/".$b."\">");
+                    }
+                ?>
+            </div>
+        </div>
+    </body>
+
+</html>
